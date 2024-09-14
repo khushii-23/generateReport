4c804815330fe1115b3e4b035f1f663d7fc70344
Khushi Bhakar <168291038+khushii-23@users.noreply.github.com>
2024-09-14 22:25:27 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react'
-import Home from './components/Home'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
+import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
+    <Router> {/* Wrap Routes with BrowserRouter */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
